3d765e827c8cd8d338b787e3633dbddd2042b133
Zack Brenton <zack@jnickel.com>
2017-10-15 18:14:25 -0300

    initial commit

---
+++ src/Latency.php
@@ -0,0 +1,54 @@
+<?php
+namespace PromSR7;
+
+use Prometheus\CollectorRegistry;
+use Prometheus\Histogram;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Latency
+{
+    /**
+     * @var Histogram
+     */
+    private $latencyHistogram;
+
+    public function __construct(
+        CollectorRegistry $collectorRegistry,
+        string $namespace,
+        array $buckets = [0.005, 0.01, 0.05, 0.10, 0.25, 0.40, 0.70, 1]
+    ) {
+        $this->latencyHistogram = $collectorRegistry->getOrRegisterHistogram(
+            $namespace,
+            'request_duration_seconds',
+            'The duration of requests, in seconds',
+            ['path'],
+            $buckets
+        );
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable $next
+     *
+     * @return ResponseInterface
+     */
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        callable $next
+    ) {
+        $startTime = microtime(true);
+        $response = $next($request, $response);
+
+        $latency = microtime(true) - $startTime;
+        error_log(sprintf("latency: %f, endpoint: %s", $latency, $request->getUri()->getPath()));
+        $this->latencyHistogram->observe(
+            $latency,
+            [$request->getUri()->getPath()]
+        );
+
+        return $response;
+    }
+}
